e2bc7e3515cfa38062eb77be6029cf896e3023c9
Felipe Eduardo <48593133+devfelipeeduardo@users.noreply.github.com>
2025-10-13 07:53:00 -0300

    Update Nav.jsx
    Teste se funciona

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -32,7 +32,7 @@ function Nav() {
                 <a href="#third-section" className="nav-menu-option"><ul>Serviços</ul></a>
                 <a href="#fourth-section" className="nav-menu-option"><ul>Depoimentos</ul></a>
             </li>
-            <button className="nav-button-schedule">
+            <button href="https://api.whatsapp.com/send/?phone=5511975126282&text&type=phone_number&app_absent=0" className="nav-button-schedule">
                 AGENDAR CONSULTA
             </button>
         </nav>
