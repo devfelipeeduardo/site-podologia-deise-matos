c4c9e8f08567ac2288c8f828e2b9799c047be462
Felipe <felipeeduardofreitasm@gmail.com>
2025-10-22 18:06:07 -0300

    remove: apenas remocao de codigo a mais que estava quebrando o deploy

--- frontend/src/components/ScheduleButton/ScheduleButton.jsx
+++
@@ -1,9 +0,0 @@
-import "./ScheduleButton.module.css"
-
-function ScheduleButton() {
-  return (
-    <button className="schedule-button">ScheduleButton</button>
-  )
-}
-
-export default ScheduleButton;
